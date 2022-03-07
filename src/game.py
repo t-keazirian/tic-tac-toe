2a906ed1c26439b0650524410cd9f3abe6c38eb5
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-07 17:06:59 -0600

    added updates to test and src file

--- src/game.py
+++ src/game.py
@@ -1,9 +1,11 @@
 class Game:
 
     def hello_world(self):
-        print('Welcome to Tic Tac Toe!')
         return 'Hello, world!'
+
+    def display_welcome_message(self):
+        print('Welcome to Tic Tac Toe')
 
 if __name__ == '__main__':
     game = Game()
-    game.hello_world()
+    game.display_welcome_message()
