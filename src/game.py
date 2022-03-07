55f8b503a25ab8f957d43d9905de4bd8f696e88a
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-07 15:17:58 -0600

    added calling function to display welcome message

--- src/game.py
+++ src/game.py
@@ -1,4 +1,9 @@
 class Game:
 
     def hello_world(self):
+        print('Welcome to Tic Tac Toe!')
         return 'Hello, world!'
+
+if __name__ == '__main__':
+    game = Game()
+    game.hello_world()
