0026e1dde9c660a14fe4b089b996133ce94918b9
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-09 10:43:23 -0600

    added linter and fixed formatting issue

--- src/game.py
+++ src/game.py
@@ -1,12 +1,11 @@
 class Game:
-
     def hello_world(self):
-        return 'Hello, world!'
+        return "Hello, world!"
 
     def display_welcome_message(self):
-        print('Welcome to Tic Tac Toe')
+        print("Welcome to Tic Tac Toe")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     game = Game()
     game.display_welcome_message()
