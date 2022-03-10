568a14f11ce89b48be3e4bf27f933eae62f766b7
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-10 15:24:08 -0600

    added multiple print statement tests

--- test/test_game.py
+++ test/test_game.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 from src.game import Game
 
@@ -8,10 +9,15 @@ class TestGame(unittest.TestCase):
         subject = Game().hello_world
         self.assertEqual(subject(), "Hello, world!")
 
-    def test_welcome_message(self):
-        subject = Game().hello_world
-        self.assertEqual(subject(), "Hello, world!")
+    @patch("builtins.print")
+    def test_welcome_message(self, mock_print):
+        Game().display_welcome_message()
+        mock_print.assert_called_with("Welcome to Tic Tac Toe")
 
-    def test_print_board(self):
-        subject = Game().hello_world
-        self.assertEqual(subject(), "Hello, world!")
+    @patch("builtins.print")
+    def test_print_board(self, mock_print):
+        board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        Game().display_board()
+        mock_print.assert_called_with(
+            f"{board[0]} | {board[1]} | {board[2]}\n--+--+--\n{board[3]} | {board[4]} | {board[5]}\n--+--+--\n{board[6]} | {board[7]} | {board[8]}",
+        )
