17f8c8d524433e70709b02d715a37f5a014ea44c
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-15 09:04:58 -0500

    added playerX goes first, tests for playerX

--- src/game.py
+++ src/game.py
@@ -8,6 +8,17 @@ class Game:
     def initialize_board(self):
         return f"{self.board[0]} | {self.board[1]} | {self.board[2]}\n--+--+--\n{self.board[3]} | {self.board[4]} | {self.board[5]}\n--+--+--\n{self.board[6]} | {self.board[7]} | {self.board[8]}"
 
+    def handle_prompt_message(self):
+        return "Choose a number 1-9 to input your player: "
+
+    def handle_first_player(self):
+        position_choice = input()
+        position_choice = int(position_choice) - 1
+        self.board[position_choice] = "X"
+        print(self.initialize_board())
+
     def run(self):
         print(self.get_welcome_message())
         print(self.initialize_board())
+        print(self.handle_prompt_message())
+        self.handle_first_player()
--- test/test_game.py
+++ test/test_game.py
@@ -14,3 +14,17 @@ class TestGame(unittest.TestCase):
             "1 | 2 | 3\n--+--+--\n4 | 5 | 6\n--+--+--\n7 | 8 | 9",
             game.initialize_board(),
         )
+
+    def test_handle_prompt_message(self):
+        game = Game()
+        self.assertEqual(
+            "Choose a number 1-9 to input your player: ", game.handle_prompt_message()
+        )
+
+    def test_X_displays_in_index_0_when_1_is_inputted(self):
+        game = Game()
+        Game().handle_first_player()
+        self.assertEqual(
+            "X | 2 | 3\n--+--+--\n4 | 5 | 6\n--+--+--\n7 | 8 | 9",
+            game.initialize_board(),
+        )
