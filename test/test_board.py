4819e30c9342d28e1069ff06afbeeaf8bef05e1b
Taylor Keazirian <tkeazirian@8thlight.com>
2022-03-25 11:49:12 -0500

    updated method name

--- test/test_board.py
+++ test/test_board.py
@@ -7,31 +7,31 @@ class TestBoard(unittest.TestCase):
     def test_is_full_returns_true_if_board_is_full(self):
         board = Board()
         full_board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
-        total_marks_on_board = board.count_marks_in_board(full_board, "X", "O")
+        total_marks_on_board = board.count_marks(full_board, "X", "O")
         self.assertEqual(True, board.is_full(total_marks_on_board, full_board))
 
     def test_is_full_returns_false_if_board_isnt_full(self):
         board = Board()
         full_board = ["X", "2", "3", "4", "X", "O", "X", "O", "X"]
-        total_marks_on_board = board.count_marks_in_board(full_board, "X", "O")
+        total_marks_on_board = board.count_marks(full_board, "X", "O")
         self.assertEqual(False, board.is_full(total_marks_on_board, full_board))
 
     def test_no_turns_taken_yet(self):
         board = Board()
         current_board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        total_marks_on_board = board.count_marks_in_board(current_board, "X", "O")
+        total_marks_on_board = board.count_marks(current_board, "X", "O")
         self.assertEqual(0, total_marks_on_board)
 
     def test_one_turn_taken_returns_one_mark_on_board(self):
         board = Board()
         current_board = ["X", "2", "3", "4", "5", "6", "7", "8", "9"]
-        total_marks_on_board = board.count_marks_in_board(current_board, "X", "O")
+        total_marks_on_board = board.count_marks(current_board, "X", "O")
         self.assertEqual(1, total_marks_on_board)
 
     def test_two_turns_taken_returns_two_marks_on_board(self):
         board = Board()
         current_board = ["X", "O", "3", "4", "5", "6", "7", "8", "9"]
-        total_marks_on_board = board.count_marks_in_board(current_board, "X", "O")
+        total_marks_on_board = board.count_marks(current_board, "X", "O")
         self.assertEqual(2, total_marks_on_board)
 
     def test_zero_turns_player_x_goes_first(self):
