b7fb0b0a3980f75c9036240bb0dacd5f1b2c2841
Taylor Keazirian <tkeazirian@8thlight.com>
2022-04-13 11:16:08 -0500

    simplified user_interface

--- src/user_interface.py
+++ src/user_interface.py
@@ -5,15 +5,6 @@ class UserInterface:
     def display_board(self, board):
         print(board)
 
-    def get_move(self):
-        user_input = input()
-        return user_input
-
-    def get_play_again_user_input(self):
-        user_input = input()
-        return user_input
-
     def get_user_input(self):
         user_input = input()
         return user_input
-
--- test/test_user_interface.py
+++ test/test_user_interface.py
@@ -4,11 +4,6 @@ from unittest.mock import patch
 from src.user_interface import UserInterface
 
 
-class MockMessage:
-    def invalid_board_input(self):
-        return "Incorrect board input"
-
-
 class TestUserInterface(unittest.TestCase):
     @patch("builtins.print")
     def test_display_message_prints_welcome_message_passed_in(self, mock_print):
@@ -17,20 +12,21 @@ class TestUserInterface(unittest.TestCase):
         user_interface.display_message(message)
         mock_print.assert_called_with("Welcome to Tic Tac Toe")
 
-    @patch("builtins.input", side_effect=["3"])
-    def test_get_move_returns_input(self, mock_input):
+    @patch("builtins.print")
+    def test_display_board_prints_board(self, mock_print):
         user_interface = UserInterface()
-        result = user_interface.get_move()
+        board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        user_interface.display_board(board)
+        mock_print.assert_called_with(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
+
+    @patch("builtins.input", side_effect=["3"])
+    def test_get_user_input_returns_input(self, mock_input):
+        user_interface = UserInterface()
+        result = user_interface.get_user_input()
         self.assertEqual("3", result)
 
     @patch("builtins.input", side_effect=["5"])
     def test_gets_user_input_function_not_returning_false_positive(self, mock_input):
         user_interface = UserInterface()
-        output = user_interface.get_move()
+        output = user_interface.get_user_input()
         self.assertNotEqual(output, "3")
-
-    @patch("builtins.input", side_effect=["Y"])
-    def test_get_play_again_user_input_returns_input(self, mock_input):
-        user_interface = UserInterface()
-        output = user_interface.get_play_again_user_input()
-        self.assertEqual("Y", output)
