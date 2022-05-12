ee65baaf79c9c1631c64b657a145f5a47719a7f3
Taylor Keazirian <tkeazirian@8thlight.com>
2022-05-12 15:56:10 -0500

    refactored test_ui with setup

--- test/test_user_interface.py
+++ test/test_user_interface.py
@@ -5,28 +5,27 @@ from src.user_interface import UserInterface
 
 
 class TestUserInterface(unittest.TestCase):
+    def setUp(self):
+        self.ui = UserInterface()
+
     @patch("builtins.print")
     def test_display_message_prints_welcome_message_passed_in(self, mock_print):
-        user_interface = UserInterface()
         message = "Welcome to Tic Tac Toe"
-        user_interface.display_message(message)
+        self.ui.display_message(message)
         mock_print.assert_called_with("Welcome to Tic Tac Toe")
 
     @patch("builtins.print")
     def test_display_board_prints_board(self, mock_print):
-        user_interface = UserInterface()
         board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        user_interface.display_board(board)
+        self.ui.display_board(board)
         mock_print.assert_called_with(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
 
     @patch("builtins.input", side_effect=["3"])
     def test_get_user_input_returns_input(self, mock_input):
-        user_interface = UserInterface()
-        result = user_interface.get_user_input()
+        result = self.ui.get_user_input()
         self.assertEqual("3", result)
 
     @patch("builtins.input", side_effect=["5"])
     def test_gets_user_input_function_not_returning_false_positive(self, mock_input):
-        user_interface = UserInterface()
-        output = user_interface.get_user_input()
+        output = self.ui.get_user_input()
         self.assertNotEqual(output, "3")
