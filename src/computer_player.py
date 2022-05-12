23332bd3e8f8427b1434de01b2882580a6f32f14
Taylor Keazirian <tkeazirian@8thlight.com>
2022-05-12 11:22:50 -0500

    modified minimax algo

--- src/computer_player.py
+++ src/computer_player.py
@@ -5,8 +5,6 @@ from src.rules import Rules
 from src.human_player import HumanPlayer
 from src.board import Board
 
-# from src.game import Game
-
 # from src.minimax import Minimax
 
 
@@ -16,58 +14,54 @@ class ComputerPlayer:
         # self.minimax = Minimax()
 
     def get_move(self, board, message):
-        move = random.randint(1, 9)
+        # move = random.randint(1, 9)
         # move = self.best_move(board)
-        if not validator.spot_is_available(self, board, move):
-            return self.get_move(board, message)
-        return move
+        # if not validator.spot_is_available(self, board, move):
+        #     return self.get_move(board, message)
+        # return move
+        return self.best_move_fn(board)
 
-    def best_move(self, board):
+    def best_move_fn(self, board):
         best_score = -math.inf
-        # best_move = None
+        best_move = None
         for mark in board:
-            print(mark, "mark")
-            if not Board().is_spot_taken(board, mark):
-                board[int(mark)] = self.mark
+            if mark != "X" and mark != "O":
+                board[int(mark) - 1] = "X"
                 score = self.minimax(board, 0, False)
-                # reset the mark to the index #
-                board[mark] = mark
+                board[int(mark) - 1] = mark
                 if score > best_score:
                     best_score = score
-                    move = board[mark]
-        board[move] = self.mark
+                    # best_move = board[int(mark) - 1]
+                    best_move = mark
+        board[int(best_move) - 1] = "X"
+        return int(best_move)
 
     def minimax(self, board, depth, is_maximizing):
-        scores = {"X": 10, "O": -10, "tie": 0}
-        winning_mark = Game().get_winning_mark(Game().total_marks_on_board)
+        # scores = {"X": 10, "O": -10, "tie": 0}
+        winning_mark = Rules().get_winning_mark(board)
 
-        if winning_mark == "X":
-            return scores["X"]
-        elif winning_mark == "O":
-            return scores["O"]
-        elif Rules().is_winner is False:
+        if winning_mark == "O":
+            return -10
+        elif winning_mark == "X":
+            return 10
+        elif winning_mark == None:
             return 0
-
-        # if Rules().is_winner(board):
-        #     return scores["X"]
 
         if is_maximizing:
             best_score = -math.inf
             for mark in board:
-                if not Board().is_spot_taken(board, mark):
-                    board[int(mark)] = self.mark
+                if mark != "X" and mark != "O":
+                    board[int(mark) - 1] = "X"
                     score = self.minimax(board, depth + 1, False)
-                    # reset the mark to the index #
-                    board[mark] = mark
+                    board[int(mark) - 1] = mark
                     best_score = max(score, best_score)
             return best_score
         else:
             best_score = math.inf
             for mark in board:
-                if not Board().is_spot_taken(board, mark):
-                    board[int(mark)] = self.mark
+                if mark != "X" and mark != "O":
+                    board[int(mark) - 1] = "O"
                     score = self.minimax(board, depth + 1, True)
-                    # reset the mark to the index #
-                    board[mark] = mark
+                    board[int(mark) - 1] = mark
                     best_score = min(score, best_score)
             return best_score
